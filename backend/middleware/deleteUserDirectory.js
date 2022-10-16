b19845cb9e692c2006c47c892dae8860d9802bbd
Devin Brueberg <brueberd@gmail.com>
2022-10-16 16:29:10 -0500

    All image file CRUD operations now use Cloudinary instead of AWS

--- backend/middleware/deleteUserDirectory.js
+++ backend/middleware/deleteUserDirectory.js
@@ -3,75 +3,43 @@
 // Restaurant Club - deleteUserDirectory.js
 // March 11, 2022
 // Last Edited (Initials, Date, Edits):
+//  (DAB, 10/16/2022, Updated code from AWS to Cloudinary)
 
-const aws = require('aws-sdk');
-const checkEnv = require("../helperFunction/checkEnvironment")
+// Cloudinary for deleting images
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
 
-// Check if we are on the prod environment
-const isProd = checkEnv();
+// The deleteUserDirectory async function will accept a directory
+// (in this case it is the userId) and delete all the images and 
+// videos uploaded from that user
+const deleteUserDirectory = async(directory) => {
+    // Appending users to get the full directory path
+    const fullDirectoryPath = `users/${directory}`;
 
-// Set S3 endpoint to DigitalOcean Spaces
-const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
-const s3 = new aws.S3({
-    accessKeyId: isProd ? process.env.S3_KEY : process.env.aws_access_key_id,
-    secretAccessKey: isProd ? process.env.S3_SECRET : process.env.aws_secret_access_key,
-    endpoint: spacesEndpoint
-});
+    // DEBUG *****
+    // console.log("FULL DIRECTORY PATH: ", fullDirectoryPath);
 
-/**
- *  S3 does not delete empty directories so we have to go in and delete everything first
- * 
- * @param {*} directory 
- * @returns 
- */
-const deleteUserDirectory = async (directory) => {
+    // Deleting any videos in the directory
+    await cloudinary.api.delete_resources_by_prefix(fullDirectoryPath, {
+        invalidate: true, resource_type: "video"
+    })
+        .then(result => console.log(result))
+        .catch(error => console.log(error));
 
-    const params = {
-        Bucket: 'restaurantclub',
-        Prefix: 'users/' + directory
-    };
-
-    try {
-        // Here we get and store a list of files currently in the users/:id directory
-        const listedFiles = await s3.listObjectsV2(params).promise();
-        
-        // console.log("listedFiles: ", listedFiles)
-
-        // Directory is already empty, do nothing
-        if (listedFiles.Contents.length === 0) {
-            console.log("directory is empty")
-            return -3;
-        }
-
-        // If the directory was found, we must delete everything inside first to remove it
-        try {
-            const deleteParams = {
-                Bucket: 'restaurantclub',
-                Delete: { Objects: [] }
-            };
-
-            // Add each of the directory files to the delete array
-            listedFiles.Contents.forEach(({ Key }) => {
-                deleteParams.Delete.Objects.push({ Key });
-            });
-
-            await s3.deleteObjects(deleteParams).promise();
-
-            // console.log("files: ", deleteParams);
-
-            console.log("directory deleted Successfully")
+    // Deleting any images in the directory and returning the result to the caller
+    return await cloudinary.api.delete_resources_by_prefix(fullDirectoryPath, {
+        invalidate: true
+    })
+        .then(result => {
+            // If the delete was successful 1 is returned
+            console.log("Directory successfully deleted", result)
             return 1;
-        }
-        // Something went wrong trying to delete the directory
-        catch (err) {
-            console.log("Error deleting directory : " + JSON.stringify(err));
+        })
+        .catch(error => {
+            // If there was an error, that error and -2 is returned
+            console.log("Error deleting directory", error)
             return -2;
-        }
-        // Otherwise the directory was not found
-    } catch (err) {
-        console.log("directory not Found ERROR : " + err.code);
-        return -1;
-    }
-};
+        });
+}
 
 module.exports = deleteUserDirectory;
--- backend/testCredentials.js
+++ backend/testCredentials.js
@@ -1,4 +1,6 @@
+
 const cloudinaryPublicIDFromURL = require('./helperFunction/utilityHelper');
+const deleteUserDirectory = require('./middleware/deleteUserDirectory');
 
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
@@ -17,16 +19,29 @@ const cloudinary = require('cloudinary').v2;
 //             return -2;
 //         });
 
-const rawURL = "https://res.cloudinary.com/slen/image/upload/v1665928436/users/5/ervsh9eh78mlt5hxhg7m.jpg";
-const formattedURL = (rawURL) => {
-    const locationArray = rawURL.split('https://res.cloudinary.com/slen/image/upload/')
-    const endURL = locationArray[1];
-    const removeKeyURL = endURL.substring(endURL.indexOf("/") + 1) || endURL;
-    console.log(removeKeyURL);
-    const finalRoute = removeKeyURL.substring(0, removeKeyURL.lastIndexOf(".")) || removedKeyURL;
-    console.log(finalRoute);
-    const test = cloudinaryPublicIDFromURL(rawURL, 'https://res.cloudinary.com/slen/image/upload/');
-    console.log(test);
-}
+// const rawURL = "https://res.cloudinary.com/slen/image/upload/v1665928436/users/5/ervsh9eh78mlt5hxhg7m.jpg";
+// const formattedURL = (rawURL) => {
+//     const locationArray = rawURL.split('https://res.cloudinary.com/slen/image/upload/')
+//     const endURL = locationArray[1];
+//     const removeKeyURL = endURL.substring(endURL.indexOf("/") + 1) || endURL;
+//     console.log(removeKeyURL);
+//     const finalRoute = removeKeyURL.substring(0, removeKeyURL.lastIndexOf(".")) || removedKeyURL;
+//     console.log(finalRoute);
+//     const test = cloudinaryPublicIDFromURL(rawURL, 'https://res.cloudinary.com/slen/image/upload/');
+//     console.log(test);
+// }
 
-formattedURL(rawURL);
+// formattedURL(rawURL);
+
+// cloudinary.api.delete_resources_by_prefix("users/5", {invalidate: true, resource_type: "video"})
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error));
+
+const results = async(directory) => {
+    approved = await deleteUserDirectory(directory);
+    console.log("results of deleteUserDirectory", approved);
+};
+
+results(6);
+
+
